d0fbc2046aeab705b41c4dcd1d59ee54058cb8fc
Li Liu <Li@deMacBook-Pro-2.local>
2024-08-09 14:31:22 -0400

    Add Header and URL Query

--- snippetbox/main.go
+++ snippetbox/main.go
@@ -1,22 +1,46 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 )
 
 // Define home handler writes a byte slice
 // "Hello from Snippetbox" as res
 func home(w http.ResponseWriter, r *http.Request) {
+	// Checks the URL and render 404 if needed
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
 	w.Write([]byte("Hello from Snippetbox"))
 }
 
 // Add a showSnippet handler function
 func showSnippet(w http.ResponseWriter, r *http.Request) {
+	id, err := strconv.Atoi(r.URL.Query().Get("id"))
+	if err != nil || id < 1 {
+		http.NotFound(w, r)
+		return
+	}
+
+	fmt.Fprintf(w, "Display a specific snippet with ID %d...", id)
 	w.Write([]byte("Display a specific snippet..."))
 }
 
 func createSnippet(w http.ResponseWriter, r *http.Request) {
+	// Restrict createSnippet to take only POST request
+	if r.Method != "POST" {
+		w.Header().Set("Allow", "POST")
+		// WriteHeader and Write can only be called once
+		// Body and Header modifications need to happen before it
+		http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
+		// w.WriteHeader(405)
+		// w.Write([]byte("Method Not Allowed"))
+		return
+	}
 	w.Write([]byte("Create a snippet..."))
 }
 
